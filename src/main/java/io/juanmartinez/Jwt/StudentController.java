abaad9f54e128c5284a913936f7c569fe18157bd
Juan Martinez <juanmartinez5678@hotmail.com>
2024-08-24 19:47:28 -0400

    added a new controller to the repo

---
+++ src/main/java/io/juanmartinez/Jwt/StudentController.java
@@ -0,0 +1,34 @@
+package io.juanmartinez.Jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    private List<Student> students = new ArrayList<>(List.of(
+           new Student(1,"Juan",60),
+            new Student(2,"Sarah",65)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+//this is how you can get a token from spring-boot security to be able to make requests
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+}
